5b6f5c23ece01898710064fb4fd96d84f49651cf
Vignesh3030 <vigneshvigu3030@gmail.com>
2024-12-20 19:03:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Ninja Bikes navigation page loaded!');
+    // Placeholder for future JavaScript functionalities.
+});
